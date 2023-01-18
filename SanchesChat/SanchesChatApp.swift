0061740a18f2e70d54e6608fb9f55a55aabd7693
sanches37 <xognsl87@gmail.com>
2023-01-18 16:21:25 +0900

    Initial Commit

---
+++ SanchesChat/SanchesChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanchesChatApp.swift
+//  SanchesChat
+//
+//  Created by tae hoon park on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct SanchesChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
