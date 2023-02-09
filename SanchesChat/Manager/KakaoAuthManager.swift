f7ab91b1dff789c786513e04105aa368ed5c6dc6
sanches37 <xognsl87@gmail.com>
2023-02-09 09:04:19 +0900

    kakaoManager의 코드를 Combine을 사용하도록 변경

--- SanchesChat/Manager/KakaoAuthManager.swift
+++ SanchesChat/Manager/KakaoAuthManager.swift
@@ -5,57 +5,49 @@
 //  Created by tae hoon park on 2023/01/18.
 //
 
-import Foundation
+import Combine
 import KakaoSDKUser
 import KakaoSDKAuth
 
 struct KakaoAuthManager {
-  func getKakaoToken(completion: @escaping (Result<OAuthToken, KakaoAuthError>) -> Void) {
-    if AuthApi.hasToken() {
-      UserApi.shared.logout { error in
-        if let error = error {
-          completion(.failure(.unknown(description: error.localizedDescription)))
+  func getKakaoToken() -> AnyPublisher<OAuthToken, KakaoAuthError> {
+    return Future<OAuthToken, KakaoAuthError> { promise in
+      if AuthApi.hasToken() {
+        UserApi.shared.logout { error in
+          if let error = error {
+            promise(.failure(.unknown(description: error.localizedDescription)))
+          }
+          checkedKakaoInstallState { promise($0) }
         }
-        checkedKakaoInstallState { result in
-          completion(.success(result))
-        }
+      } else {
+        checkedKakaoInstallState { promise($0) }
       }
-    } else {
-      checkedKakaoInstallState { result in
-        completion(.success(result))
-      }
-    }
+    }.eraseToAnyPublisher()
   }
   
-  private func checkedKakaoInstallState(completion: @escaping ((OAuthToken) -> Void)) {
-    if UserApi.isKakaoTalkLoginAvailable() {
-      UserApi.shared.loginWithKakaoTalk { oAuthToken, error in
-        do {
-          completion(try self.onReceiveKakaoToken(oAuthToken: oAuthToken, error: error))
-        } catch {
-          debugPrint(error)
+  private func checkedKakaoInstallState(
+    completion: @escaping (Result<OAuthToken, KakaoAuthError>) -> Void) {
+      if UserApi.isKakaoTalkLoginAvailable() {
+        UserApi.shared.loginWithKakaoTalk { oAuthToken, error in
+          completion(onReceiveKakaoToken(oAuthToken: oAuthToken, error: error))
         }
-      }
-    } else {
-      UserApi.shared.loginWithKakaoAccount { oAuthToken, error in
-        do {
-          completion(try self.onReceiveKakaoToken(oAuthToken: oAuthToken, error: error))
-        } catch {
-          debugPrint(error)
+      } else {
+        UserApi.shared.loginWithKakaoAccount { oAuthToken, error in
+          completion(onReceiveKakaoToken(oAuthToken: oAuthToken, error: error))
         }
       }
     }
-  }
   
-  private func onReceiveKakaoToken(oAuthToken: OAuthToken?, error: Error?) throws -> OAuthToken {
-    if let error = error {
-      throw KakaoAuthError.unknown(description: error.localizedDescription)
-    } else {
+  private func onReceiveKakaoToken(
+    oAuthToken: OAuthToken?,
+    error: Error?) -> Result<OAuthToken, KakaoAuthError> {
+      if let error = error {
+        return .failure(.unknown(description: error.localizedDescription))
+      }
       guard let kakaoToken = oAuthToken else {
-        throw KakaoAuthError.tokenLookupFailed
+        return .failure(.tokenLookupFailed)
       }
       print("카카오톡 로그인 성공: \(kakaoToken)")
-      return kakaoToken
+      return .success(kakaoToken)
     }
-  }
 }
